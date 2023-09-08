72e26d62dfdde8c3d1648772987bef08ec5290b8
godfredayiku <godfredayikumacauley@gmail.com>
2023-09-07 20:05:57 -0700

    Byte size task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("Size of int: %lu byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	printf("Size of a char: %u byte(s)", sizeof(char));
+	printf("Size of int: %u byte(s)", sizeof(int));
+	printf("Size of a long int: %u byte(s)", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)", sizeof(long long int));
+	printf("Size of a float: %u byte(s)", sizeof(float));
 	return (0);
 }
