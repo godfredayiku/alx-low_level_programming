2446d46d022c2ab373e38beaa2ff7076225e599c
godfredayiku <godfredayikumacauley@gmail.com>
2023-09-07 19:34:11 -0700

    Hello, printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: returns 0 with success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
