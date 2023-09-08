1c82a7f455108c6b87e59eb9b4b04e1247e29757
godfredayiku <godfredayikumacauley@gmail.com>
2023-09-07 19:27:38 -0700

    Hello, puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function of the program
+ *
+ * Return: indicates that the program run without any errors
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
