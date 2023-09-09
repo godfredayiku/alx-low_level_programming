8776b5d577f4f52b48d696bd9faee16a431e24ef
godfredayiku <godfredayikumacauley@gmail.com>
2023-09-08 18:48:41 -0700

    Smile in the mirror task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
+
