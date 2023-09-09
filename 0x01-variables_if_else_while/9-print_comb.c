c84525d61803b8dbd00114e44acd8889bd3eebe1
godfredayiku <godfredayikumacauley@gmail.com>
2023-09-08 19:06:05 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
