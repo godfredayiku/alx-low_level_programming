b7b037a56b3fed77154acfc80428e5dfb576c3e1
godfredayiku <godfredayikumacauley@gmail.com>
2023-09-08 19:50:50 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	int d1 = 0;
+	int d2, d3;
+
+	while (d1 <= 9)
+	{
+		d2 = 0;
+		while (d2 <= 9)
+		{
+			d3 = 0;
+			while (d3 <= 9)
+			{
+				if (d1 != d2 && d1 < d2 && d2 != d3 && d2 < d3)
+				{
+					putchar(d1 + 48);
+					putchar(d2 + 48);
+					putchar(d3 + 48);
+
+					if (d1 + d2 + d3 != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				d3++;
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
