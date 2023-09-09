6900a36de17e810f8de98624f64e55ec4904521b
godfredayiku <godfredayikumacauley@gmail.com>
2023-09-08 18:43:35 -0700

    Numberz task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ * Return: returns 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+
+	return (0);
+}
+
