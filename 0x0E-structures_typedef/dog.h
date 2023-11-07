034292fd5a27610f2e77123ef2726bab06e6d35d
godfredayiku <godfredayikumacauley@gmail.com>
2023-11-07 09:42:28 -0800

    Made changes to dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,19 +9,12 @@
  * Description: Longer description
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef for struct dog
-*/
-
-typedef struct dog dog_t;
-
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
